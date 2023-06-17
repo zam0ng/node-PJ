bba4a4b84586790b047fbbffb8499422a6746e03
zam0ng <sinze27@gmail.com>
2023-06-17 17:08:18 +0900

    24

--- backend/routers/books.js
+++ backend/routers/books.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {createBooks,UserUpload} = require("../controllers/booksContoller");
 const { Upload } = require("../img");
-const {islogin2} =require("../controllers/islogin");
+const {islogin2,islogin} =require("../controllers/islogin");
 
 // router.get('/',createBooks)
 router.post('/',Upload.single("upload"),islogin,UserUpload)
