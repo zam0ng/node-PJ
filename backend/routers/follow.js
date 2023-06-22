d0959741cad0c3f40833ef7ecfed0eb74baee9bc
zam0ng <sinze27@gmail.com>
2023-06-22 11:08:30 +0900

    1

--- backend/routers/follow.js
+++ backend/routers/follow.js
@@ -3,6 +3,6 @@ const {islogin2,islogin} = require("../controllers/islogin");
 const {followingList,followadd, followdel} = require("../controllers/followingController")
 
 router.get('/list',islogin,followingList)
-// router.get('/add',islogin,followadd)
+router.get('/add',islogin,followadd)
 router.get('/del',islogin,followdel)
 module.exports = router;
