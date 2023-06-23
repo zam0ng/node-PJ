d3c12e9483087bf8d2d157064593fbf4d94c7ec6
zam0ng <sinze27@gmail.com>
2023-06-23 13:24:15 +0900

    1

--- backend/routers/mainRouter.js
+++ backend/routers/mainRouter.js
@@ -5,7 +5,7 @@ const { islogin,islogin2,adminislogin,viewislogin} =require("../controllers/islo
 router.get("/viewlist", OrderByView);
 router.get("/starlist", OrderByStar);
 router.get("/logincheck",islogin,logincheck);
-router.get("/admincheck",adminislogin);
+router.get("/admincheck",islogin,adminislogin);
 router.get("/viewcheck",viewislogin);
 // router.get('/',(req,res)=>{
 //     res.redirect("/http://13.209.64.80/frontend/index.html");
