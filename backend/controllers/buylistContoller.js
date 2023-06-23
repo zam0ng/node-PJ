273e2a54ad0102f05c7b923bea7e9c8769b0d054
zam0ng <sinze27@gmail.com>
2023-06-23 15:40:18 +0900

    1

--- backend/controllers/buylistContoller.js
+++ backend/controllers/buylistContoller.js
@@ -3,13 +3,10 @@ exports.buyList = async(req,res)=>{
     try {
         const {user_id}=req.decoded;
         const data = await User.findOne({ where: {user_id:user_id, }, raw:true});
-        console.log(data.buys);
         const as = data.buys;
         const zx = as.split(",");
        let q ="";
         if (data.buys == "") {
-            console.log
-            console.log( req.params.id)
           q = req.params.id;
         } else {
         q= data.buys
@@ -23,8 +20,6 @@ exports.buyList = async(req,res)=>{
         const data2 = await Books.findAll({
             where :{id:zx},raw:true
         })
-        console.log("buylist contoller=====================")
-        console.log(data2);
         res.json(data2)
     } catch (error) {
         console.log("buylist contoller에서 오류났음")
