1204be6f7ce17cbb2a9d2c9f1b8639c3f741dfc9
zam0ng <sinze27@gmail.com>
2023-06-23 11:22:13 +0900

    1

--- backend/controllers/islogin.js
+++ backend/controllers/islogin.js
@@ -45,17 +45,6 @@ exports.adminislogin = async (req, res) => {
     
     const { access_token } = req.session;
     
-    if(access_token){
-      const {user_id} = req.session;
-
-      if(user_id !="admin"){
-        res.send("undi")
-      }
-      else{
-        res.send();
-      }
-
-    }
     console.log(access_token);
     console.log(access_token==undefined)
     console.log(access_token=="undefined" || user_id !="")
